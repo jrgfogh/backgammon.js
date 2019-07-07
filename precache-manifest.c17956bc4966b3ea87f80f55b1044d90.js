a5d1027bce16af9ac1c014ed19a5de1c52c9a350
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-07 18:27:10 +0000

    Deploy jrgfogh/backgammon.js to github.com/jrgfogh/backgammon.js.git:gh-pages

---
+++ precache-manifest.c17956bc4966b3ea87f80f55b1044d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9914dc4de8537d876cd08e178db6a93",
+    "url": "/backgammon.js/index.html"
+  },
+  {
+    "revision": "05f8fdde3310c9214172",
+    "url": "/backgammon.js/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/backgammon.js/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "05f8fdde3310c9214172",
+    "url": "/backgammon.js/static/js/main.d9f0e88e.chunk.js"
+  },
+  {
+    "revision": "d6ae7c60b0071d77d9d2",
+    "url": "/backgammon.js/static/js/runtime~main.fd64aa03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/backgammon.js/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/backgammon.js/precache-manifest.c17956bc4966b3ea87f80f55b1044d90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/backgammon.js/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
